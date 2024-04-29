a467094b3d37871cd18300383d65066f26c04513
Sh-karan27 <shuklakaran107@gmail.com>
2024-04-29 19:17:49 +0530

    Edited Timeline element

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,16 @@ import Hero from './Hero';
 import Services from './Services';
 
 const Home = () => {
-  return <>
-  <section className='flex flex-col items-center justify-center w-full h-full'>
-  <Hero/>
-  <Services/>
-  </section>
-  
-  
-  </>;
+  return (
+    <>
+      <section className='flex flex-col items-center justify-center w-full h-full'>
+        <Hero />
+        <div className='bg-[#f5f5f5] w-full mt-10'>
+          <Services />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,76 +1,86 @@
-import slide1 from "../assets/HeroImg.webp";
-import slide2 from "../assets/App Development.jpg";
-import slide3 from "../assets/Coding.jpg";
-import slide4 from "../assets/Software Development.avif";
+import slide1 from '../assets/HeroImg.webp';
+import slide2 from '../assets/App Development.jpg';
+import slide3 from '../assets/Coding.jpg';
+import slide4 from '../assets/Software Development.avif';
+import TechSolutios from '../assets/TechSolutios.jpg';
+import SoftwareSales from '../assets/SoftwareSales.png';
+import WebsiteDevelopment from '../assets/WebsiteDevelopment.png';
+import Softwaredevelopment from '../assets/Softwaredevelopment.png';
+import ProfessionalTraining from '../assets/ProfessionalTraining.webp';
+import TechSupport from '../assets/TechSupport.png';
+import CloudInfrastructure from '../assets/Cloud-infrastructure.jpg';
+import Webscraping from '../assets/Webscraping.png';
+import AIDataAnalytics from '../assets/AIDataAnalytics.png';
+import Research from '../assets/Research.jpeg';
 
-export const NavList = ["home", "about", "services", "blog", "shop", "contact"];
+export const NavList = ['home', 'about', 'services', 'blog', 'shop', 'contact'];
 
 export const heroSlider = [
-  { description: "Your Vision, Our Code", src: slide1 },
+  { description: 'Your Vision, Our Code', src: slide1 },
   { description: "Building Tomorrow's Technology Today", src: slide2 },
-  { description: "Crafting Innovative Solutions through Code", src: slide3 },
-  { description: "Transforming Ideas into Digital Reality", src: slide4 },
+  { description: 'Crafting Innovative Solutions through Code', src: slide3 },
+  { description: 'Transforming Ideas into Digital Reality', src: slide4 },
 ];
 
-export  const services = [
+export const services = [
   {
-    name: "Tech Solutions",
+    name: 'Tech Solutions',
     description:
-      "Sales, repairs, and support for the latest products, including computers, laptops, & accessories. Everything you need for your tech lifestyle.",
-    image: "",
+      'Sales, repairs, and support for the latest products, including computers, laptops, & accessories. Everything you need for your tech lifestyle.',
+    image: TechSolutios,
   },
   {
-    name: "Software Sales",
+    name: 'Software Sales',
     description:
-      "A wide range of essential software solutions for both personal and business use, including productivity tools, antivirus, and accounting software.",
-    image: "",
+      'A wide range of essential software solutions for both personal and business use, including productivity tools, antivirus, and accounting software.',
+    image: SoftwareSales,
   },
   {
-    name: "Website Development",
+    name: 'Website Development',
     description:
-      "Elevate your online presence with our custom website development and integrated digital marketing strategies for unparalleled digital success.",
-    image: "",
+      'Elevate your online presence with our custom website development and integrated digital marketing strategies for unparalleled digital success.',
+    image: WebsiteDevelopment,
   },
   {
-    name: "Software Development",
+    name: 'Software Development',
     description:
-      "Custom software solutions and database management, featuring cloud applications, API integrations, and expertise in SQL and NoSQL systems.",
-    image: "",
+      'Custom software solutions and database management, featuring cloud applications, API integrations, and expertise in SQL and NoSQL systems.',
+    image: Softwaredevelopment,
   },
   {
-    name: "Professional Training",
+    name: 'Professional Training',
     description:
-      "Workshops and training in AI, machine learning, data science, coding, and web design, complemented by cutting-edge research and development.",
-    image: "",
+      'Workshops and training in AI, machine learning, data science, coding, and web design, complemented by cutting-edge research and development.',
+    image: ProfessionalTraining,
   },
   {
-    name: "Tech Support",
+    name: 'Tech Support',
     description:
       "Round-the-clock support for all your technology and software setup needs, ensuring you're always equipped and supported, wherever, whenever.",
-    image: "",
+    image: TechSupport,
   },
   {
-    name: "Cloud & IT Infrastructure",
+    name: 'Cloud & IT Infrastructure',
     description:
-      "Secure, scalable cloud computing services and robust IT support, including network management and cybersecurity, for a protected digital environment.",
-    image: "",
+      'Secure, scalable cloud computing services and robust IT support, including network management and cybersecurity, for a protected digital environment.',
+    image: CloudInfrastructure,
   },
   {
-    name: "Web Scraping",
+    name: 'Web Scraping',
     description:
-      "Custom web scraping to gather, analyze, and leverage web data for enhanced market research and strategic business insights.",
-    image: "",
+      'Custom web scraping to gather, analyze, and leverage web data for enhanced market research and strategic business insights.',
+    image: Webscraping,
   },
   {
-    name: "AI & Data Analytics",
+    name: 'AI & Data Analytics',
     description:
-      "Advanced solutions in AI, machine learning, computer vision, and comprehensive data analytics to transform data into actionable insights.",
-    image: "",
+      'Advanced solutions in AI, machine learning, computer vision, and comprehensive data analytics to transform data into actionable insights.',
+    image: AIDataAnalytics,
   },
   {
-    name: "Research & Development (R&D)",
+    name: 'Research & Development (R&D)',
     description:
-      "Innovating future technologies with our dedicated R&D team, focused on creating scalable, sustainable and forward-thinking solutions.",
-    image: "",
+      'Innovating future technologies with our dedicated R&D team, focused on creating scalable, sustainable and forward-thinking solutions.',
+    image: Research,
   },
 ];
