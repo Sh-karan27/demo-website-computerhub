89cbcf58633d82531eb11f2e85bcca2273d6b62d
Sh-karan27 <shuklakaran107@gmail.com>
2024-04-30 14:19:59 +0530

    added  error page content ps: final submit

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Error = () => {
-  return <div>Error</div>;
+  return <section className='w-full h-full flex items-center justify-center'>
+    <div className='w-full h-[65vh] flex flex-col items-center justify-center gap-10'>
+<h1 className='text-5xl text-blue-500'>404 error</h1>
+<h2 className='text-3xl text-blue-500'>This pagr does not exist.</h2>
+
+    </div>
+  </section>;
 };
 
 export default Error;
